4725332a33138bd1c9ea516b6ecc5613a63a7481
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-12-26 12:15:35 -0800

    removed redirects from next.config

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
     experimental:{
         serverActions: true
     } ,
-    async redirects() {
-        return [
-          {
-            source: '/',
-            destination: '/',
-            permanent: false,
-          },
-        ]
-      },
 }
 
 module.exports = nextConfig
