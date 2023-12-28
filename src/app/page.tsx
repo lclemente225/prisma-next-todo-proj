7f94382839901c4292dd1a7d12d32de904340fe6
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-12-27 21:13:27 -0800

    added header to home page error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,9 +66,21 @@ export default async function Home(){
     console.error('Error fetching todos:', error);
 
     return (
+      <>
+      <header className=' flex justify-between items-center mb-4'>
+          <h1 className="text-2xl">
+            Todos
+          </h1>
+          <Link href="/new" className='border border-slate-300 text-slate-300 px-2 py-1
+          rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'
+          >
+            New
+          </Link>
+        </header>
       <div className="text-red-500">
         An error occurred while fetching todos. Please try again later.
       </div>
+      </>
     );
   }
 }
